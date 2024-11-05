b467d23dc9301c8f3280a0662b8a19963654fae3
Charles Hill <charlesgdhill@gmail.com>
2024-11-04 20:40:56 -0800

    set up form and generate files for CRUD operations

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,5 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 0) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_05_042022) do
+  create_table "todos", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
